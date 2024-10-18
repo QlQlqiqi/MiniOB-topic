1f1660dc2062402460327d9a305435db2ce5abd6
yqzhang4480 <yqzhang4480@qq.com>
2024-10-18 21:40:05 +0800

    stmt: update clause

--- src/observer/sql/stmt/update_stmt.cpp
+++ src/observer/sql/stmt/update_stmt.cpp
@@ -20,7 +20,37 @@ UpdateStmt::UpdateStmt(Table *table, Value *values, int value_amount)
 
 RC UpdateStmt::create(Db *db, const UpdateSqlNode &update, Stmt *&stmt)
 {
-  // TODO
-  stmt = nullptr;
-  return RC::INTERNAL;
+  const char* table_name = update.relation_name.c_str();
+  if (nullptr == db || nullptr == table_name) {
+    LOG_WARN("invalid argument. db=%p, table_name=%p",
+        db, table_name);
+    return RC::INVALID_ARGUMENT;
+  }
+
+  // check whether the table exists
+  Table* table = db->find_table(table_name);
+  if (nullptr == table)
+  {
+    LOG_WARN("no such table. db=%s, table_name=%s", db->name(), table_name);
+    return RC::SCHEMA_TABLE_NOT_EXIST;
+  }
+
+  // check the schema
+  const TableMeta&              table_meta  = table->table_meta();
+  const std::vector<FieldMeta>* field_metas = table_meta.field_metas();
+  for (auto& field_meta : *field_metas)
+  {
+    if (strcmp(field_meta.name(), update.attribute_name.c_str()) == 0)
+    {
+      if (auto ftype = field_meta.type(), vtype = update.value.attr_type(); ftype != vtype)
+      {
+        LOG_WARN("schema mismatch. field type: %d, value type: %d", static_cast<int>(ftype), static_cast<int>(vtype));
+        return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+      }
+    }
+  }
+
+  // everything alright
+  stmt = new UpdateStmt(table, &update.value, 1);
+  return RC::SUCCESS;
 }
