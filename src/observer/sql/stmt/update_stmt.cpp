4a223608efedd48c0891f36e8dd9e0c8cf7ee3f4
张英奇 Zhang Yingqi <zyq4480@qq.com>
2024-10-20 11:14:27 +0800

    update (#1)
    * stmt: update clause
    
    * operator,plan: update clause. WIP
    
    * stmt,logical: update clause
    
    * physical: update clause
    
    * fix(logical): update clause
    
    * logical,physical: update clause.

--- src/observer/sql/stmt/update_stmt.cpp
+++ src/observer/sql/stmt/update_stmt.cpp
@@ -13,14 +13,74 @@ See the Mulan PSL v2 for more details. */
 //
 
 #include "sql/stmt/update_stmt.h"
+#include "common/log/log.h"
+#include "storage/db/db.h"
+#include "storage/table/table.h"
 
-UpdateStmt::UpdateStmt(Table *table, Value *values, int value_amount)
-    : table_(table), values_(values), value_amount_(value_amount)
+UpdateStmt::UpdateStmt(Table *table, const Value *values, int value_amount, const FieldMeta *field_metas,
+    int field_amount, FilterStmt *stmt)
+    : table_(table),
+      values_(values),
+      value_amount_(value_amount),
+      field_metas_(field_metas),
+      field_amount_(field_amount),
+      filter_stmt_(stmt)
 {}
+
+UpdateStmt::~UpdateStmt()
+{
+  if (nullptr != filter_stmt_) {
+    delete filter_stmt_;
+    filter_stmt_ = nullptr;
+  }
+}
 
 RC UpdateStmt::create(Db *db, const UpdateSqlNode &update, Stmt *&stmt)
 {
-  // TODO
-  stmt = nullptr;
-  return RC::INTERNAL;
+  const char *table_name = update.relation_name.c_str();
+  if (nullptr == db || nullptr == table_name) {
+    LOG_WARN("invalid argument. db=%p, table_name=%p",
+        db, table_name);
+    return RC::INVALID_ARGUMENT;
+  }
+
+  // check whether the table exists
+  Table *table = db->find_table(table_name);
+  if (nullptr == table) {
+    LOG_WARN("no such table. db=%s, table_name=%s", db->name(), table_name);
+    return RC::SCHEMA_TABLE_NOT_EXIST;
+  }
+
+  // check the schema
+  const TableMeta              &table_meta    = table->table_meta();
+  const std::vector<FieldMeta> *field_metas   = table_meta.field_metas();
+  const FieldMeta              *to_be_updated = nullptr;
+  for (auto &field_meta : *field_metas) {
+    if (strcmp(field_meta.name(), update.attribute_name.c_str()) == 0) {
+      if (auto ftype = field_meta.type(), vtype = update.value.attr_type(); ftype != vtype) {
+        LOG_WARN("schema mismatch. field type: %d, value type: %d", static_cast<int>(ftype), static_cast<int>(vtype));
+        return RC::SCHEMA_FIELD_TYPE_MISMATCH;
+      }
+      // Only single update supported for now.
+      to_be_updated = &field_meta;
+      break;
+    }
+  }
+
+  // filter
+  std::unordered_map<std::string, Table *> table_map;
+  table_map.insert(std::pair<std::string, Table *>(std::string(table_name), table));
+
+  FilterStmt *filter_stmt = nullptr;
+
+  RC rc = FilterStmt::create(
+      db, table, &table_map, update.conditions.data(), static_cast<int>(update.conditions.size()), filter_stmt);
+  if (rc != RC::SUCCESS) {
+    LOG_WARN("failed to create filter statement. rc=%d:%s", rc, strrc(rc));
+    return rc;
+  }
+
+  // everything alright
+  stmt = new UpdateStmt(table, &update.value, 1, to_be_updated, 1, filter_stmt);
+  return RC::SUCCESS;
 }
--- src/observer/sql/stmt/update_stmt.h
+++ src/observer/sql/stmt/update_stmt.h
@@ -16,6 +16,8 @@ See the Mulan PSL v2 for more details. */
 
 #include "common/rc.h"
 #include "sql/stmt/stmt.h"
+#include "storage/field/field.h"
+#include "sql/stmt/filter_stmt.h"
 
 class Table;
 
@@ -27,18 +29,28 @@ class UpdateStmt : public Stmt
 {
 public:
   UpdateStmt() = default;
-  UpdateStmt(Table *table, Value *values, int value_amount);
+  UpdateStmt(Table *table, const Value *values, int value_amount, const FieldMeta *field_metas, int field_amount,
+      FilterStmt *filter_stmt);
+
+  ~UpdateStmt() override;
 
 public:
   static RC create(Db *db, const UpdateSqlNode &update_sql, Stmt *&stmt);
+  StmtType  type() const override { return StmtType::UPDATE; }
 
 public:
-  Table *table() const { return table_; }
-  Value *values() const { return values_; }
-  int    value_amount() const { return value_amount_; }
+  Table           *table() const { return table_; }
+  const Value     *values() const { return values_; }
+  int              value_amount() const { return value_amount_; }
+  const FieldMeta *field_metas() const { return field_metas_; }
+  int              field_amount() const { return field_amount_; }
+  FilterStmt      *filter_stmt() const { return filter_stmt_; }
 
 private:
-  Table *table_        = nullptr;
-  Value *values_       = nullptr;
-  int    value_amount_ = 0;
+  Table           *table_        = nullptr;
+  const Value     *values_       = nullptr;
+  int              value_amount_ = 0;
+  const FieldMeta *field_metas_  = nullptr;
+  int              field_amount_ = 0;
+  FilterStmt      *filter_stmt_  = nullptr;
 };
